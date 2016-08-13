ef20b91fa61e6ea2d2f04612ef3c01f24ede6713
Naveed Jooma <naveedjooma@gmail.com>
2016-08-13 19:14:50 -0400

    adding capsule wrapper

---
+++ discoveryengine/capsule/api_manager.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+import requests
+
+from discoveryengine.settings import CAPSULE_ID, CAPSULE_API_KEY
+
+from .models import *
+
+class APIManager(object):
+
+    def addPerson(Person):
--- discoveryengine/discoveryengine/settings.py
+++ discoveryengine/discoveryengine/settings.py
@@ -171,3 +171,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Capsule Integration
+CAPSULE_ID = get_env_var("CAPSULE_ID")
+CAPSULE_API_KEY = get_env_var("CAPSULE_API_KEY")
