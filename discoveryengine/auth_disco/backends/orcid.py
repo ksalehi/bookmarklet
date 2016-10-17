162cda55d55c8ce91f2770f9cf281188d7939f35
Naveed Jooma <njooma@Naveeds-MacBook-Pro-2.local>
2016-10-17 16:15:34 -0400

    getting ORCID backend to work for authentication scope only

--- discoveryengine/auth_disco/backends/orcid.py
+++ discoveryengine/auth_disco/backends/orcid.py
@@ -4,12 +4,16 @@ class ORCIDOAuth2(BaseOAuth2):
     """ORCID OAuth2 authentication backend"""
     AUTHORIZATION_URL = 'https://sandbox.orcid.org/oauth/authorize'
     SCOPE_SEPARATOR = ' '
+    ACCESS_TOKEN_METHOD = 'POST'
 
-    def auth_complete_params(self, state=None):
-        data = super(ORCIDOAuth2, self).auth_complete_params(state)
-        print data
-        return data
-
+    def get_user_details(self, response):
+        print response
+        return {
+            'username': response.get('orcid'),
+            'first_name': response.get('name').split(' ')[0],
+            'last_name': response.get('name').split(' ')[-1],
+            'orcid': response.get('orcid'),
+        }
 
 class ORCIDPublicOAuth2(ORCIDOAuth2):
     """ORCID OAuth2 authentication backend for Public API"""
