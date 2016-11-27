21b6cae8c8807f4f0b31d728aa0c893db075999c
Kia Salehi <ksalehi@wellesley.edu>
2016-11-27 10:35:24 -0800

    first bookmarklet text

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,7 @@
 console.log('loaded bmlet.js');
+
+const doi = document.getElementsByTagName('doi');
+
+const popUpWindow = document.createTextNode("This is the fucking header text!");
+
+document.getElementsByTagName('body')[0].appendChild(popUpWindow);
