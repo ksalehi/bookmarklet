874a5f8a59f626fe08c937baad45fbbe6cbc02f0
Kia Salehi <ksalehi@wellesley.edu>
2016-12-03 14:32:33 -0800

    crawl pubmed dom and open new window

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,7 +1,31 @@
 console.log('loaded bmlet.js');
 
-const doi = document.getElementsByTagName('doi');
+let doi;
 
-const popUpWindow = document.createTextNode("This is the fucking header text!");
+document.getElementsByTagName('meta').forEach( metaTag => {
+  const name = metaTag.getAttribute('name');
+  if (name === 'citation_doi' || 'prism_doi') {
+    console.log('found citation_doi metatag');
+    doi = metaTag.getAttribute('content');
+    console.log(doi);
+  }
+});
 
-document.getElementsByTagName('body')[0].appendChild(popUpWindow);
+if (!doi) {
+  // pubmed
+  document.getElementsByTagName('dd').forEach( ddTag => {
+    const doiTag = ddTag.child('a');
+    if (doiTag) {
+      if (doiTag.getAttribute('ref') === 'aid_type=doi') {
+        doi = doiTag.getAttribute('href');
+        console.log('found dd / a tag');
+        console.log(doi);
+      }
+    }
+  });
+}
+
+console.log('this is updating!');
+
+const url = 'rate.thediscoverengine.org' + '?doi=' + doi;
+window.open(url);
