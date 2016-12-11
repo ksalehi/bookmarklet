1285f774af801aa2f18cdde5320d3f1974ca5079
Naveed Jooma <naveed.jooma@thediscoveryengine.org>
2016-12-11 14:04:51 -0500

    scoping the variables

--- discoveryengine/bookmarklet/static/bookmarklet/js/bookmarklet.js
+++ discoveryengine/bookmarklet/static/bookmarklet/js/bookmarklet.js
@@ -2,36 +2,40 @@
 
 console.log('Welcome to the Discovery Engine via the amazing bookmarklet! Please email feedback@thediscoveryengine.org with any suggestions!');
 
-let doi;
-const possibleDoiTagNames = {
-  'citation_doi': true,
-  'prism_doi': true,
-  'dc.identifier.doi': true,
-  'bepress_citation_doi': true,
-  'ppl.doi': true,
-  'doi': true
-};
+function perform() {
+  let doi;
+  const possibleDoiTagNames = {
+    'citation_doi': true,
+    'prism_doi': true,
+    'dc.identifier.doi': true,
+    'bepress_citation_doi': true,
+    'ppl.doi': true,
+    'doi': true
+  };
 
 
-Array.from(document.getElementsByTagName('meta')).forEach( metaTag => {
-  let tagName = metaTag.getAttribute('name');
-  if (possibleDoiTagNames[tagName]) {
-    doi = metaTag.getAttribute('content');
+  Array.from(document.getElementsByTagName('meta')).forEach( metaTag => {
+    let tagName = metaTag.getAttribute('name');
+    if (possibleDoiTagNames[tagName]) {
+      doi = metaTag.getAttribute('content');
+    }
+  });
+
+  if (!doi) {
+    /*** CUSTOM SITE PARSERS ***/
+    // PubMed
+    if (window.location.hostname === 'www.ncbi.nlm.nih.gov') {
+      const ddTags = document.getElementsByTagName('dd');
+      doi = ddTags[1].childNodes[0].innerText;
+      console.log('pubmed doi:');
+      console.log(doi);
+    } else {
+      doi = 'DOI_NOT_FOUND';
+    }
   }
-});
 
-if (!doi) {
-  /*** CUSTOM SITE PARSERS ***/
-  // PubMed
-  if (window.location.hostname === 'www.ncbi.nlm.nih.gov') {
-    const ddTags = document.getElementsByTagName('dd');
-    doi = ddTags[1].childNodes[0].innerText;
-    console.log('pubmed doi:');
-    console.log(doi);
-  } else {
-    doi = 'DOI_NOT_FOUND';
-  }
+  let url = 'http://rate.thediscoveryengine.org' + '?doi=' + doi;
+  window.open(url);
 }
 
-let url = 'http://rate.thediscoveryengine.org' + '?doi=' + doi;
-window.open(url);
+perform();
